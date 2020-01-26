8f974fe104fb37a246a9696a87509faf85c49d36
yuki lin <christinelinyunyun8@gmail.com>
2020-01-27 00:52:30 +0800

    test2

--- Test/Assets/NewBehaviourScript.cs
+++ Test/Assets/NewBehaviourScript.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
