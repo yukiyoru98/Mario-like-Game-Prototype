d61931c341bb6090827e686bff9b7c09c8a8823f
yuki lin <christinelinyunyun8@gmail.com>
2020-03-23 10:42:13 +0800

    added GameStart scene and LoadingScenes

---
+++ Test/Assets/Scripts/GameStart/StartCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCtrl : MonoBehaviour
+{
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Return)){
+            LoadingScenes.self.ChangeScene("Main", 0.5f);
+        }    
+    }
+}
---
+++ Test/Assets/Scripts/LoadingScenes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScenes : MonoBehaviour
+{
+    public static LoadingScenes self;
+    private void Awake()
+    {
+        self = this;
+    }
+
+    private string SceneName = "";
+    private Animation anim;
+
+    private void Start() {
+        anim = this.GetComponent<Animation>();
+    }
+
+    public void ChangeScene(string scene, float delay)
+    {
+        SceneName = scene;
+        anim.Play("FadeOut");
+        Invoke("GoToScene", delay);
+    }
+
+    void GoToScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
