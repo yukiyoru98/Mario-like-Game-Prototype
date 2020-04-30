31c0d0326e6dbee4b89cceaeebb73eee2503d69e
yuki lin <christinelinyunyun8@gmail.com>
2020-04-30 08:07:56 +0800

    added MakerFlicker and Select Object

--- Mario_Like_Game_Prototype/Assets/Scripts/Maker/MakerCtrl.cs
+++ Mario_Like_Game_Prototype/Assets/Scripts/Maker/MakerCtrl.cs
@@ -2,7 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MakerCtrl : MonoBehaviour //Editor/SceneObjects/Maker
+public class MakerCtrl : MonoBehaviour //Maker/SceneObjects/Maker
 {
-    
+    public static MakerCtrl self;
+    private void Awake() {
+        self = this;
+    }
+    private int TargetIdx = 0; //the child index of currently displaying object
+    public GameObject Target; //currently displaying object
+    private int ObjCount;
+
+    private void Start() {
+        ObjCount = this.transform.childCount;
+        Target = transform.GetChild(TargetIdx).gameObject;
+        Target.SetActive(true);
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Z)){
+            Switch(-1);
+        }
+        if(Input.GetKeyDown(KeyCode.X)){
+            Switch(1);
+        }
+    }
+
+    void Switch(int next){
+        Target.SetActive(false);
+        TargetIdx += next;
+        if(TargetIdx < 0){
+            TargetIdx = ObjCount;
+        }
+        else if(TargetIdx == ObjCount ){
+            TargetIdx = 0;
+        }
+        Target = transform.GetChild(TargetIdx).gameObject;
+        Target.SetActive(true);
+    }
 }
---
+++ Mario_Like_Game_Prototype/Assets/Scripts/Maker/MakerFlicker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakerFlicker : MonoBehaviour //Maker/SceneObjects/Maker
+{
+    private bool FlickerBool = true;
+    private void Start() {
+        Invoke("Flicker", 0.5f);
+    }
+    
+    void Flicker(){
+        FlickerBool = !FlickerBool;
+        MakerCtrl.self.Target.SetActive(FlickerBool);
+        Invoke("Flicker", 0.5f);
+    }
+}
